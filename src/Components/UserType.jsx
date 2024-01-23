7c4cab6f05c339e5045b1b1354425fdcd331f90d
TheRabbi <tipson664@gmail.com>
2024-01-23 23:15:49 +0100

    signup form update

---
+++ src/Components/UserType.jsx
@@ -0,0 +1,33 @@
+function UserType({ setType }) {
+  const handleUserTypeSelection = (selectedType) => {
+    setType(selectedType);
+  };
+  return (
+    <div className="flex flex-col w-full h-fit my-24">
+      <div className="w-6/12">
+        <input
+          name="firstName"
+          type="button"
+          className="w-full  border border-black py-8 px-3 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Firstname"
+          value="Farmer"
+          onClick={()=>handleUserTypeSelection("FARMER")}
+          required
+        />
+      </div>
+      <div className="w-6/12 py-4">
+        <input
+          name="firstName"
+          type="button"
+          className="w-full  border border-black py-8 px-3 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Firstname"
+          value="User"
+          onClick={()=>handleUserTypeSelection("uSER")}
+          required
+        />
+      </div>
+    </div>
+  );
+}
+
+export default UserType;
