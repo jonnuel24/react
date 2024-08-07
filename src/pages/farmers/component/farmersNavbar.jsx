9114a93dadade19b51d2071f2f21d53d4fe1228d
Jonnuel24 <ekachikwu9@gmail.com>
2024-08-07 11:16:59 +0100

    view product page

--- src/pages/farmers/component/farmersNavbar.jsx
+++ src/pages/farmers/component/farmersNavbar.jsx
@@ -12,40 +12,40 @@ function FNavbar() {
   const navigate = useNavigate();
   const user = useSelector((state) => state.user?.currentUser);
   const isAuthenticated = useSelector((state) => state.user?.isAuthenticated);
+
   useEffect(() => {
     if (isAuthenticated === false) {
       navigate("/login");
     }
   }, [user, isAuthenticated, navigate]);
+
   return (
     <div>
       <div className="fnav bg-[#000000] px-16 py-6 w-full flex flex-row justify-between">
-        <Link to={"/website"}>
+        <Link to={"/website"} className="no-underline">
           <img src={Logo} alt="" />
         </Link>
 
         <div className="flex flex-row items-center gap-[21px]">
-          <Icon icon="uil:comment-message" className="fn-icon" />
-          <Icon icon="solar:bell-linear" className="fn-icon" />
-          <div className="flex flex-row items-center gap-[21px]">
-            <div className="fn-profile">
-              <img src={pic} alt="" className="animal" />
+          {/* <Icon icon="uil:comment-message" className="fn-icon" /> */}
+          <Link to="/notifications" className="no-underline cursor-pointer">
+            <Icon icon="solar:bell-linear" className="fn-icon" />
+          </Link>
+          <Link to="/settings" className="no-underline flex">
+            <div className="flex flex-row items-center gap-[21px]">
+              <div className="fn-profile">
+                <img src={pic} alt="" className="animal" />
+              </div>
+              <div className="flex flex-row items-center gap-[4px]">
+                <label
+                  htmlFor=""
+                  className="text-white text-[20px] font-medium leading-normal"
+                >
+                  {user?.firstName}
+                </label>
+              </div>
             </div>
-            <div className="flex flex-row items-center gap-[4px]">
-              <label
-                htmlFor=""
-                className="text-white text-[20px] font-medium leading-normal"
-              >
-                {user?.firstName}
-              </label>
-              <Icon
-                icon="majesticons:chevron-down"
-                color="white"
-                width="24px"
-                height="21px"
-              />
-            </div>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
